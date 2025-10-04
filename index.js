c9cf22514fe663d984bba3cc7fd932ab75cee0de
ale <tomescualexandramirela@gmail.com>
2025-10-04 13:22:23 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.static(__dirname + "/public"))
+
+app.get("/", (req, res) => {
+    res.send("hello world!")
+})
+
+app.get("/ping", (req, res) => {
+    res.send("pong")
+})
+
+app.listen(8080)
